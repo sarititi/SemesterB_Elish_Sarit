a439a4b2bffdb96e8dbee476f98935040fb50707
sarititi <sartn1621@gmail.com>
2026-05-06 08:27:57 +0300

    שינווים פיצים לא להתייחס

--- server/app.js
+++ server/app.js
@@ -8,16 +8,28 @@ import commentsRoutes from './routes/commentsRoutes.js';
 
 const app = express();
 
+app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5174',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
 }));
 
+app.get('/', (req, res) => {
+    res.send('Server is running 🚀');
+});
 app.options('*', cors());
 
+app.use('/users', usersRoutes);
+app.use('/todos', todosRoutes);
+app.use('/posts', postsRoutes);
+app.use('/comments', commentsRoutes);
 app.use(express.json());
 
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+//לא באמת מוכן
 app.use((req, res, next) => {
     console.log('REQ:', req.method, req.url);
     next();
